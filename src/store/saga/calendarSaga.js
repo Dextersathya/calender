6e625a1b6f8fc906030fb29d31edd44bb568638a
sathya <vtu20618@veltech.edu.in>
2025-01-03 19:36:39 +0530

    commented unused codes

--- src/store/saga/calendarSaga.js
+++ src/store/saga/calendarSaga.js
@@ -1,33 +1,11 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
+import {  put, takeEvery } from 'redux-saga/effects';
 import {
   FETCH_EVENTS_REQUEST,
   // FETCH_EVENTS_SUCCESS,
   FETCH_EVENTS_FAILURE,
 } from '../constants/calendarConstants';
 
-// Mock API call
-const fetchEventsApi = () =>
-  new Promise((resolve) =>
-    setTimeout(() => {
-      resolve([
-        {
-          id: '1',
-          title: 'Email with Company A',
-          date: '2024-12-01',
-          status: 'completed',
-          details: {
-            name: 'Company A',
-            location: 'New York',
-            linkedIn: 'https://linkedin.com/company-a',
-            emails: 'contact@companya.com',
-            phoneNumbers: '1234567890',
-            comments: 'Discuss contract terms.',
-            periodicity: 'Monthly',
-          },
-        },
-      ]);
-    }, 1000)
-  );
+
 
 function* fetchEvents() {
   try {
