3322f1774e964b521b8909cb24393b47a52051e0
sathya <vtu20618@veltech.edu.in>
2025-01-03 19:29:27 +0530

    fixed bugs

--- src/store/saga/calendarSaga.js
+++ src/store/saga/calendarSaga.js
@@ -1,7 +1,7 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import {
   FETCH_EVENTS_REQUEST,
-  FETCH_EVENTS_SUCCESS,
+  // FETCH_EVENTS_SUCCESS,
   FETCH_EVENTS_FAILURE,
 } from '../constants/calendarConstants';
 
@@ -31,7 +31,7 @@ const fetchEventsApi = () =>
 
 function* fetchEvents() {
   try {
-    const events = yield call(fetchEventsApi);
+    // const events = yield call(fetchEventsApi);
     // yield put({ type: FETCH_EVENTS_SUCCESS, payload: events });
   } catch (error) {
     yield put({ type: FETCH_EVENTS_FAILURE, payload: error.message });
